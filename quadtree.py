a75726967e5996b4c9903769b1deeb1691253de9
Jan Martowicz <martowiczjan@gmail.com>
2024-12-30 14:21:48 +0100

    Dobre quadtree

--- quadtree.py
+++ quadtree.py
@@ -1,3 +1,5 @@
+
+from __future__ import annotations
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,98 +7,115 @@ from visualizer.main import Visualizer
 from geo_structures import RectangleArea, Point
 
 
+class QuadtreeNode:
+    def __init__(self, rectangle: RectangleArea):
+        self.rectangle = rectangle  # Całkowity obszar tego węzła
+        self.points = []  # Punkty w tym węźle
+        self.upper_left = None  # Lewy górny kwadrant
+        self.upper_right = None  # Prawy górny kwadrant
+        self.lower_left = None  # Lewy dolny kwadrant
+        self.lower_right = None  # Prawy dolny kwadrant
+        self.is_leaf = True  # Czy jest liściem (czy ma dzieci)
+    
+    def __str__(self):
+        return f"QuadtreeNode({self.rectangle}, Points={len(self.points)}, is_leaf={self.is_leaf})"
+
 class Quadtree:
-    def __init__(self, boundary, capacity):
-        # Inicjalizacja Quadtree z prostokątem granicznym i maksymalną pojemnością punktów
-        self.boundary = boundary  # Prostokąt definiujący granice tego Quadtree
-        self.capacity = capacity  # Maksymalna liczba punktów przed podziałem
-        self.points = []  # Lista punktów przechowywanych w tym Quadtree
-        self.divided = False  # Czy Quadtree zostało podzielone na podregiony
-
-    def subdivide(self):
-        """Dzieli Quadtree na cztery mniejsze regiony."""
-        x1, y1, x2, y2 = (
-            self.boundary.min_x,
-            self.boundary.min_y,
-            self.boundary.max_x,
-            self.boundary.max_y,
-        )
-        x = (x1 + x2) / 2
-        y = (y1 + y2) / 2
-
-        # Tworzenie czterech podregionów: górny-lewy, górny-prawy, dolny-lewy, dolny-prawy
-        self.upperleft = Quadtree(RectangleArea(x1, y, x, y2), self.capacity)
-        self.upperright = Quadtree(RectangleArea(x, y, x2, y2), self.capacity)
-        self.lowerleft = Quadtree(RectangleArea(x, y1, x2, y), self.capacity)
-        self.lowerright = Quadtree(RectangleArea(x1, y1, x, y), self.capacity)
-
-        self.divided = True  # Oznaczamy, że Quadtree zostało podzielone
-
-    def insert(self, point):
-        if not point in self.boundary:
-            return False  # Punkt znajduje się poza granicami tego Quadtree
-
-        if len(self.points) < self.capacity:
-            # Jeśli mamy miejsce, dodajemy punkt
-            self.points.append(point)
-            return True
-
-        if not self.divided:
-            # Jeśli osiągnięto pojemność, dzielimy Quadtree na podregiony
-            self.subdivide()
-
-        # Próbujemy wstawić punkt do jednego z podregionów
-        return (
-            self.upperleft.insert(point)
-            or self.upperright.insert(point)
-            or self.lowerleft.insert(point)
-            or self.lowerright.insert(point)
+    def __init__(self, points: list[Point], max_points_per_node: int = 4):
+        self.max_points_per_node = max_points_per_node
+        self.max_rectangle = RectangleArea(
+            min(points, key=lambda p: p.x).x,  # Minimalna wartość x
+            min(points, key=lambda p: p.y).y,  # Minimalna wartość y
+            max(points, key=lambda p: p.x).x,  # Maksymalna wartość x
+            max(points, key=lambda p: p.y).y,  # Maksymalna wartość y
         )
 
-    def find(self, range_rect, found_points=None):
-        """Znajduje wszystkie punkty w danym prostokącie zapytania."""
-        if found_points is None:
-            found_points = []  # Inicjalizacja listy wynikowej
+        
+        self.vis = Visualizer()
+        for point in points:
+            self.vis.add_point((point.x, point.y), color='red', s=5)
 
-        if self.boundary & range_rect is None:
-            return found_points  # Brak przecięcia, zwracamy pustą listę
+        self.root = self.build_tree(self.max_rectangle, points)
 
-        # Sprawdzamy punkty w bieżącym regionie
-        for point in self.points:
-            if point in range_rect:
-                found_points.append(point)
+    def build_tree(self, rectangle: RectangleArea, points: list[Point]) -> QuadtreeNode:
+        node = QuadtreeNode(rectangle)
 
-        # Jeśli Quadtree jest podzielone, przeszukujemy podregiony
-        if self.divided:
-            self.upperleft.find(range_rect, found_points)
-            self.upperright.find(range_rect, found_points)
-            self.lowerleft.find(range_rect, found_points)
-            self.lowerright.find(range_rect, found_points)
+        # Dodaj punkty do węzła, jeśli nie przekraczają limitu
+        if len(points) <= self.max_points_per_node:
+            node.points = points
+            self.vis.add_line_segment(((rectangle.min_x, rectangle.min_y), (rectangle.min_x, rectangle.max_y)))
+            self.vis.add_line_segment(((rectangle.max_x, rectangle.min_y), (rectangle.max_x, rectangle.max_y)))
+            self.vis.add_line_segment(((rectangle.min_x, rectangle.max_y), (rectangle.max_x, rectangle.max_y)))
+            self.vis.add_line_segment(((rectangle.min_x, rectangle.min_y), (rectangle.max_x, rectangle.min_y)))
+            return node
 
-        return found_points
+        # Inaczej, dzielimy przestrzeń na cztery ćwiartki
+        mid_x = (rectangle.min_x + rectangle.max_x) / 2
+        mid_y = (rectangle.min_y + rectangle.max_y) / 2
 
-    def visualize(self, visualizer):
-        """Wizualizuje Quadtree za pomocą klasy Visualizer."""
-        # Dodaj granice tego Quadtree jako prostokąt
-        x1 = self.boundary.min_x
-        y1 = self.boundary.min_y
-        x2 = self.boundary.max_x
-        y2 = self.boundary.max_y
+        # Tworzymy cztery podobszary
+        quadrants = [
+            RectangleArea(rectangle.min_x, rectangle.min_y, mid_x, mid_y),  # Lewy dolny
+            RectangleArea(mid_x, rectangle.min_y, rectangle.max_x, mid_y),  # Prawy dolny
+            RectangleArea(rectangle.min_x, mid_y, mid_x, rectangle.max_y),  # Lewy górny
+            RectangleArea(mid_x, mid_y, rectangle.max_x, rectangle.max_y)   # Prawy górny
+        ]
 
-        visualizer.add_line_segment(((x1, y1), (x1, y2)))
-        visualizer.add_line_segment(((x1, y1), (x2, y1)))
-        visualizer.add_line_segment(((x2, y2), (x1, y2)))
-        visualizer.add_line_segment(((x2, y2), (x2, y1)))
+        
 
-        # Dodaj punkty w tym Quadtree
-        for point in self.points:
-            visualizer.add_point((point.x, point.y), color="red")
+        # Dzielimy punkty na ćwiartki
+        quadrant_points = [[] for _ in range(4)]
+        for point in points:
+            for i, q in enumerate(quadrants):
+                if point in q:  # Punkt w obrębie danego kwadrantu
+                    quadrant_points[i].append(point)
+                    break
 
-        # Jeśli Quadtree jest podzielone, wizualizuj podregiony
-        if self.divided:
-            self.upperleft.visualize(visualizer)
-            self.upperright.visualize(visualizer)
-            self.lowerleft.visualize(visualizer)
-            self.lowerright.visualize(visualizer)
+        # Tworzymy dzieci (ćwiartki) dla węzła
+        node.is_leaf = False
+        node.lower_left = self.build_tree(quadrants[0], quadrant_points[0])
+        node.lower_right = self.build_tree(quadrants[1], quadrant_points[1])
+        node.upper_left = self.build_tree(quadrants[2], quadrant_points[2])
+        node.upper_right = self.build_tree(quadrants[3], quadrant_points[3])
 
-        return visualizer
+        self.vis.add_line_segment(((rectangle.min_x, rectangle.min_y), (rectangle.min_x, rectangle.max_y)))
+        self.vis.add_line_segment(((rectangle.max_x, rectangle.min_y), (rectangle.max_x, rectangle.max_y)))
+        self.vis.add_line_segment(((rectangle.min_x, rectangle.max_y), (rectangle.max_x, rectangle.max_y)))
+        self.vis.add_line_segment(((rectangle.min_x, rectangle.min_y), (rectangle.max_x, rectangle.min_y)))
+
+        return node
+
+    def find(self, rectangle: RectangleArea) -> list[Point]:
+        return self.__find(self.root, rectangle)
+
+    def __find(self, node: QuadtreeNode, rectangle: RectangleArea) -> list[Point]:
+        res = []
+        if node.rectangle & rectangle is None:  # Jeśli prostokąty nie mają wspólnego obszaru
+            return res
+
+        # Jeśli to liść, sprawdzamy punkty
+        if node.is_leaf:
+            res.extend([p for p in node.points if p in rectangle])
+        else:
+            # Rekurencyjnie sprawdzamy dzieci (cztery ćwiartki)
+            if node.lower_left is not None:
+                res.extend(self.__find(node.lower_left, rectangle))
+            if node.lower_right is not None:
+                res.extend(self.__find(node.lower_right, rectangle))
+            if node.upper_left is not None:
+                res.extend(self.__find(node.upper_left, rectangle))
+            if node.upper_right is not None:
+                res.extend(self.__find(node.upper_right, rectangle))
+
+        return res
+
+    def tree_print(self, node: QuadtreeNode):
+        if node is not None:
+            print(node)
+            self.tree_print(node.upper_left)
+            self.tree_print(node.upper_right)
+            self.tree_print(node.lower_left)
+            self.tree_print(node.lower_right)
+
+    def get_vis(self):
+        return self.vis
